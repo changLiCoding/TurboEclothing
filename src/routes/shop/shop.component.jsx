e8f6e4a2ad5859759c60e6ce28b5f97b36e0b625
javascriptsucks <evix1988@live.cn>
2022-08-29 21:59:03 +0800

    Upload data to firebase and CategoryPreview done

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,18 +1,24 @@
-import { useContext } from "react";
+import { Fragment, useContext } from "react";
 
-import ProductCard from "../../components/product-card/product-card.component";
-import { ProductsContext } from "../../contexts/products.context";
+import CategoryPreview from "../../components/category-preview/category-preview.component";
+import { CategoriesContext } from "../../contexts/categories.context";
 
 import './shop.styles.scss';
 
 const Shop = () => {
-    const { products } = useContext(ProductsContext);
-    return (
-        <div className="products-container">
-            {products.map((product) => (
-                <ProductCard key={product.id} product={product} />
-            ))}
-        </div>)
-}
+  const { categoriesMap } = useContext(CategoriesContext);
+  // console.log(categoriesMap);
+  return (
+    <div className="shop-container">
+      {Object.keys(categoriesMap).map((title) => {
+        const products = categoriesMap[title];
+        {/* console.log(products); */ }
+        return (
+          <CategoryPreview key={title} title={title} products={products} />
+        )
+      })}
+    </div>
+  );
+};
 
 export default Shop;
