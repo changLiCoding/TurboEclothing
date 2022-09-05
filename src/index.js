4a14f89c8c3297be9c5aee5c2b2f2b85fe6dd2f6
javascriptsucks <evix1988@live.cn>
2022-09-05 14:41:32 +0800

    redux persist and redux devtool setup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
 
+import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from 'redux-persist/integration/react'
 import { Provider } from 'react-redux';
 
 import App from './App';
 // import { UserProvider } from './contexts/user.context';
 // import { CategoriesProvider } from './contexts/categories.context';
 // import { CartProvider } from './contexts/cart.context';
-import { store } from './store/store';
+import { store, persistor } from './store/store';
 
 import './index.scss';
 
@@ -17,15 +18,17 @@ root.render(
 
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        {/* <UserProvider> */}
-        {/* <CategoriesProvider> */}
-        {/* <CartProvider> */}
-        <App />
-        {/* </CartProvider> */}
-        {/* </CategoriesProvider> */}
-        {/* </UserProvider> */}
-      </BrowserRouter>
+      <PersistGate persistor={persistor} loading={null}>
+        <BrowserRouter>
+          {/* <UserProvider> */}
+          {/* <CategoriesProvider> */}
+          {/* <CartProvider> */}
+          <App />
+          {/* </CartProvider> */}
+          {/* </CategoriesProvider> */}
+          {/* </UserProvider> */}
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
